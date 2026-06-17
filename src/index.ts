f893a3ab17a2d883335c63a8f71766f5b977360a
Martiel/HomeLab <martiellima@gmail.com>
2026-06-17 16:50:04 +0000

    feat(xmlrpc): re-exportar SascarXmlRpcClient do barrel principal

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,20 @@ export { SascarClient } from './client';
 export { AsyncQueue } from './queue';
 export * from './types';
 export * from './errors';
+
+// Re-exporta o módulo XML-RPC para `import { SascarXmlRpcClient } from 'sascar-sdk'`
+export {
+  SascarXmlRpcClient,
+  type SascarXmlRpcClientOptions,
+  SascarXmlRpcError,
+  type SascarXmlRpcFault,
+  type SascarXmlRpcCommandResult,
+  type SascarXmlRpcOperacaoResult,
+  type SascarXmlRpcPosicaoResult,
+  type SascarXmlRpcSenhaResult,
+  type SascarComandoEnviado,
+  type SascarComandoStatus,
+  type SascarComandoStatusFinal,
+  type SascarXmlRpcParam,
+  SASCAR_XMLRPC_URLS
+} from './xmlrpc';
